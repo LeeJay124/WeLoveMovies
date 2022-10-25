59fbcc9821ef4f60e967239706e7f2cbcd2b5647
Lisa Jennings <mrslmjennings@gmail.com>
2022-10-25 18:21:25 -0400

    Get movieId service/route/controller

--- src/movies/movies.controller.js
+++ src/movies/movies.controller.js
@@ -2,10 +2,34 @@ const service = require("./movies.services");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 
 async function list(req, res,next){
-    const data = await service.list();
-    res.json({data});
+    const {is_showing} = req.query;
+    console.log(is_showing);
+    if(is_showing == "true"){
+         const data = await service.isShowing();
+         res.json({data});
+    }else{
+     const data = await service.list();
+     res.json({data});
+    }
+    
+}
+async function movieExists(req, res,next){
+    const {movieId} = req.params;
+    const movie = await service.read(movieId);
+    if(movie){
+        res.locals.movie = movie;
+        return next();
+    }
+    return next({status: 404, message: `Movie does not exist`})
+
+}
+async function read(req, res, next){
+const {movieId} = req.params;
+const data = await service.read(movieId);
+res.json({data});
 }
 
 module.exports = {
     list: asyncErrorBoundary(list),
+    read: [asyncErrorBoundary(movieExists), read],
 }
--- src/movies/movies.services.js
+++ src/movies/movies.services.js
@@ -1,11 +1,27 @@
 const knex = require("../db/connection");
 
-function list(){
+function list() {
     return knex("movies").select("*");
 }
+function isShowing() {
+  return knex
+  .select("m.movie_id", "m.title", "m.runtime_in_minutes", "m.rating", "m.description", "m.image_url")
+  .from("movies as m")
+  .leftJoin("movies_theaters as mt", "m.movie_id", "=", "mt.movie_id")
+  .where({"mt.is_showing" : true})
+  .groupBy("m.movie_id")
+  .orderBy("m.movie_id");
+}
 
+function read(movie_id){
+    return knex
+    .select("*")
+    .from("movies as m")
+    .where({"m.movie_id":movie_id})
+    .first();
 
+}
 
 module.exports = {
-    list, 
+    list, isShowing, read
 }
